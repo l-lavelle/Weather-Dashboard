75adf0e76c5287e6069613383296c66398b1eca5
Lauren <lauren.m.lavelle@wmich.edu>
2023-07-10 19:37:23 -0400

    CSS updated

--- assests/js/script.js
+++ assests/js/script.js
@@ -5,6 +5,7 @@ var lat;
 var lon;
 var cityName;
 
+// clear input every new call
 function getCity() {
   var input = document.getElementById("cityInput");
   cityName = input.value;
@@ -51,48 +52,101 @@ function getApi() {
       console.log(data);
 
       //   try for each ? need last day 5
-      var timeArray = [8, 16, 24, 32, 40];
-      for (var i = 8; i < data.list.length; i += 8) {
-        console.log(data.list[i].dt_txt);
-        var furtureForecast = document.getElementById("future-forecast");
-        var date1 = document.createElement("p");
-        var futureWeatherTemp = document.createElement("p");
-        var futuretWeatherWind = document.createElement("p");
-        var futureWeatherHumidty = document.createElement("p");
-        date1.textContent = dayjs(data.list[i].dt_txt).format("MM/DD/YYYY");
-        futureWeatherTemp.textContent =
-          "Temperature: " + data.list[i].main.temp;
-        futuretWeatherWind.textContent =
-          "Wind Speed: " + data.list[i].wind.speed;
-        futureWeatherHumidty.textContent =
-          "Humidity: " + data.list[i].main.humidity;
-        furtureForecast.append(
-          date1,
-          futureWeatherTemp,
-          futuretWeatherWind,
-          futureWeatherHumidty
-        );
-      }
+      // var timeArray = [8, 16, 24, 32, 40];
+      // for (var i = 8; i < data.list.length; i += 8) {
+      //   console.log(data.list[i].dt_txt);
+      //   var furtureForecast = document.getElementById("future-forecast");
+      //   var date1 = document.createElement("p");
+      //   var futureWeatherTemp = document.createElement("p");
+      //   var futuretWeatherWind = document.createElement("p");
+      //   var futureWeatherHumidty = document.createElement("p");
+      //   date1.textContent = dayjs(data.list[i].dt_txt).format("MM/DD/YYYY");
+      //   futureWeatherTemp.textContent =
+      //     "Temperature: " + data.list[i].main.temp;
+      //   futuretWeatherWind.textContent =
+      //     "Wind Speed: " + data.list[i].wind.speed;
+      //   futureWeatherHumidty.textContent =
+      //     "Humidity: " + data.list[i].main.humidity;
+      //   furtureForecast.append(
+      //     date1,
+      //     futureWeatherTemp,
+      //     futuretWeatherWind,
+      //     futureWeatherHumidty
+      //   );
+      // }
+
+      // var todayForecast = document.getElementById("current-forecast");
+      // var nameCity = document.createElement("h2");
+      // var date = document.createElement("p");
+      // var currentWeatherTemp = document.createElement("p");
+      // var currentimage = document.createElement("img");
+      // if (data.list[0].weather[0].main === "Clouds") {
+      //   currentimage.src = "./assests/images/Cloudy.png";
+      // } else if (data.list[0].weather[0].main === "Thunderstorms") {
+      //   currentimage.src = "./assests/images/thunderstorms.png";
+      // } else if (data.list[0].weather[0].main === "Rain") {
+      //   currentimage.src = "./assests/images/Rain.png";
+      // } else if (data.list[0].weather[0].main === "Snow") {
+      //   currentimage.src = "./assests/images/Snow.png";
+      // } else if (data.list[0].weather[0].main === "Clear") {
+      //   currentimage.src = "./assests/images/Sunny.png";
+      // }
+      // var currentWeatherWind = document.createElement("p");
+      // var currentWeatherHumidty = document.createElement("p");
+      // nameCity.textContent = cityName;
+      // date.textContent = dayjs(data.list[0].dt_txt).format("MM/DD/YYYY");
+      // currentWeatherTemp.textContent = "Temperature: " + data.list[0].main.temp;
+      // currentimage;
+      // currentWeatherWind.textContent = "Wind Speed: " + data.list[0].wind.speed;
+      // currentWeatherHumidty.textContent =
+      //   "Humidity: " + data.list[0].main.humidity;
+      // todayForecast.append(
+      //   nameCity,
+      //   date,
+      //   currentimage,
+      //   currentWeatherTemp,
+      //   currentWeatherWind,
+      //   currentWeatherHumidty
+      // );
 
       var todayForecast = document.getElementById("current-forecast");
       var nameCity = document.createElement("h2");
-      var date = document.createElement("p");
-      var currentWeatherTemp = document.createElement("p");
-      var currentWeatherWind = document.createElement("p");
-      var currentWeatherHumidty = document.createElement("p");
-      nameCity.textContent = cityName;
-      date.textContent = dayjs(data.list[0].dt_txt).format("MM/DD/YYYY");
-      currentWeatherTemp.textContent = "Temperature: " + data.list[0].main.temp;
-      currentWeatherWind.textContent = "Wind Speed: " + data.list[0].wind.speed;
-      currentWeatherHumidty.textContent =
-        "Humidity: " + data.list[0].main.humidity;
-      todayForecast.append(
-        nameCity,
-        date,
-        currentWeatherTemp,
-        currentWeatherWind,
-        currentWeatherHumidty
-      );
+      getinfo(0, todayForecast);
+      function getinfo(i, docEl) {
+        var date = document.createElement("p");
+        var currentWeatherTemp = document.createElement("p");
+        var currentimage = document.createElement("img");
+        if (data.list[i].weather[i].main === "Clouds") {
+          currentimage.src = "./assests/images/Cloudy.png";
+        } else if (data.list[i].weather[i].main === "Thunderstorms") {
+          currentimage.src = "./assests/images/thunderstorms.png";
+        } else if (data.list[i].weather[i].main === "Rain") {
+          currentimage.src = "./assests/images/Rain.png";
+        } else if (data.list[i].weather[i].main === "Snow") {
+          currentimage.src = "./assests/images/Snow.png";
+        } else if (data.list[i].weather[i].main === "Clear") {
+          currentimage.src = "./assests/images/Sunny.png";
+        }
+        var currentWeatherWind = document.createElement("p");
+        var currentWeatherHumidty = document.createElement("p");
+        nameCity.textContent = cityName;
+        date.textContent = dayjs(data.list[0].dt_txt).format("MM/DD/YYYY");
+        currentWeatherTemp.textContent =
+          "Temperature: " + data.list[0].main.temp;
+        currentimage;
+        currentWeatherWind.textContent =
+          "Wind Speed: " + data.list[0].wind.speed;
+        currentWeatherHumidty.textContent =
+          "Humidity: " + data.list[0].main.humidity;
+        docEl.append(
+          nameCity,
+          date,
+          currentimage,
+          currentWeatherTemp,
+          currentWeatherWind,
+          currentWeatherHumidty
+        );
+      }
     });
 }
 
